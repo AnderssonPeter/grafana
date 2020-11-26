847355d12e84d458c787418a51414307c6d12692
Dominik Prokop <dominik.prokop@grafana.com>
2020-11-26 18:29:46 +0100

    WIP

--- packages/grafana-ui/src/components/GraphNG/GraphNG.tsx
+++ packages/grafana-ui/src/components/GraphNG/GraphNG.tsx
@@ -65,12 +65,14 @@ export const GraphNG: React.FC<GraphNGProps> = ({
   const legendItemsRef = useRef<LegendItem[]>([]);
   const hasLegend = useRef(legend && legend.displayMode !== LegendDisplayMode.Hidden);
   const alignedFrame = alignedFrameWithGapTest.frame;
+
   const compareFrames = useCallback((a: DataFrame, b: DataFrame) => {
     return compareDataFrameStructures(a, b, ['min', 'max']);
   }, []);
   const configRev = useRevision(alignedFrame, compareFrames);
   const configBuilder = useMemo(() => {
     const builder = new UPlotConfigBuilder();
+    console.log('new uilder');
     // X is the first field in the alligned frame
     const xField = alignedFrame.fields[0];
     if (xField.type === FieldType.time) {
